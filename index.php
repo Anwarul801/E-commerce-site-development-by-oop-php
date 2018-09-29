b8b2ef9087a98182ba6ac2c823c0d5151b3e13c4
Anwarul801 <anwarul801@gmail.com>
2018-09-29 11:52:14 +0600

    Initial commit

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include('inc/header.php');?>
+			<!--slider-->
+<?php include('inc/slider.php');?>			
+			<!--content-->
+<?php include('inc/product_items.php');?>				
+			<!--markup footer-->
+<?php include('inc/footer.php');?>				

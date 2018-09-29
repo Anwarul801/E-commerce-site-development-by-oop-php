7fd5a6486bfc4ebaa67a48f335a2ff18ec72779a
Anwarul801 <anwarul801@gmail.com>
2018-09-29 15:54:32 +0600

    xtml template
    admin pannel and ecomerce template customice

---
+++ category_no_products.php
@@ -0,0 +1,106 @@
+<?php include("inc/header.php");?>	
+			<!--breadcrumbs-->
+			<section class="breadcrumbs">
+				<div class="container">
+					<ul class="horizontal_list clearfix bc_list f_size_medium">
+						<li class="m_right_10 current"><a href="#" class="default_t_color">Home<i class="fa fa-angle-right d_inline_middle m_left_10"></i></a></li>
+						<li><a href="#" class="default_t_color">Men</a></li>
+					</ul>
+				</div>
+			</section>
+			<!--content-->
+			<div class="page_content_offset">
+				<div class="container">
+					<div class="row clearfix">
+						<!--left content column-->
+						<section class="col-lg-9 col-md-9 col-sm-9">
+							<h2 class="tt_uppercase color_dark m_bottom_30">Men</h2>
+							<div class="clearfix m_bottom_30">
+								<figure>
+									<div class="photoframe shadow wrapper f_left f_xs_none d_xs_inline_b m_right_30 r_corners m_sm_bottom_10">
+										<img class="tr_all_long_hover" src="images/man_img_1.jpg" alt="">
+									</div>
+									<figcaption class="f_size_large p_top_10">
+										<p class="m_bottom_15">Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Donec sit amet eros. Lorem ipsum dolor sit amet, consecvtetuer adipiscing elit. Mauris fermentum dictum magna. </p>
+										<p>Sed laoreet aliquam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit. Aenean auctor wisi et urna. Aliquam erat volutpat. Duis ac turpis. Integer rutrum ante eu lacus.Vestibulum libero nisl, porta vel, scelerisque eget, malesuada at, neque. Vivamus eget nibh. Etiam cursus leo vel. </p>
+									</figcaption>
+								</figure>
+							</div>
+							<div class="alert_box r_corners info m_bottom_10 m_xs_bottom_30">
+								<i class="fa fa-info-circle"></i><p>There are no products in this category.</p>
+							</div>
+						</section>
+						<!--right column-->
+						<aside class="col-lg-3 col-md-3 col-sm-3">
+							<!--widgets-->
+							<figure class="widget shadow r_corners wrapper m_bottom_30">
+								<figcaption>
+									<h3 class="color_light">Categories</h3>
+								</figcaption>
+								<div class="widget_content">
+									<!--Categories list-->
+									<ul class="categories_list">
+										<li class="active">
+											<a href="#" class="f_size_large scheme_color d_block relative">
+												<b>Women</b>
+												<span class="bg_light_color_1 r_corners f_right color_dark talign_c"></span>
+											</a>
+											<!--second level-->
+											<ul>
+												<li class="active">
+													<a href="#" class="d_block f_size_large color_dark relative">
+														Dresses<span class="bg_light_color_1 r_corners f_right color_dark talign_c"></span>
+													</a>
+													<!--third level-->
+													<ul>
+														<li><a href="#" class="color_dark d_block">Evening Dresses</a></li>
+														<li><a href="#" class="color_dark d_block">Casual Dresses</a></li>
+														<li><a href="#" class="color_dark d_block">Party Dresses</a></li>
+													</ul>
+												</li>
+												<li>
+													<a href="#" class="d_block f_size_large color_dark relative">
+														Accessories<span class="bg_light_color_1 r_corners f_right color_dark talign_c"></span>
+													</a>
+												</li>
+												<li>
+													<a href="#" class="d_block f_size_large color_dark relative">
+														Tops<span class="bg_light_color_1 r_corners f_right color_dark talign_c"></span>
+													</a>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#" class="f_size_large color_dark d_block relative">
+												<b>Men</b>
+												<span class="bg_light_color_1 r_corners f_right color_dark talign_c"></span>
+											</a>
+											<!--second level-->
+											<ul class="d_none">
+												<li>
+													<a href="#" class="d_block f_size_large color_dark relative">
+														Shorts<span class="bg_light_color_1 r_corners f_right color_dark talign_c"></span>
+													</a>
+													<!--third level-->
+													<ul class="d_none">
+														<li><a href="#" class="color_dark d_block">Evening</a></li>
+														<li><a href="#" class="color_dark d_block">Casual</a></li>
+														<li><a href="#" class="color_dark d_block">Party</a></li>
+													</ul>
+												</li>
+											</ul>
+										</li>
+										<li>
+											<a href="#" class="f_size_large color_dark d_block relative">
+												<b>Kids</b>
+												<span class="bg_light_color_1 r_corners f_right color_dark talign_c"></span>
+											</a>
+										</li>
+									</ul>
+								</div>
+							</figure>
+						</aside>
+					</div>
+				</div>
+			</div>
+<?php include("inc/footer.php");?>
